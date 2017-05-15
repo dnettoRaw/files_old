a390d073cd0c79a380470dd6a8f4c63bb3f62003
Daniel NETTO <dnetto@e3r9p6.42.fr>
2017-05-15 19:31:23 +0200

    gnl 99% erreur de derniere minute

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,30 +6,30 @@
 /*   By: dnetto <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/11 15:36:16 by dnetto            #+#    #+#             */
-/*   Updated: 2017/05/15 15:21:23 by dnetto           ###   ########.fr       */
+/*   Updated: 2017/05/15 19:27:31 by dnetto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int		ft_check_static(char **stock, char **line)
+static int		ft_check_static(char **static_, char **line)
 {
 	char		*tmp;
 
 	tmp = NULL;
-	if (*stock)
+	if (*static_)
 	{
-		if ((tmp = ft_strchr(((const char*)*stock), '\n')))
+		if ((tmp = ft_strchr((const char*)*static_, '\n')))
 		{
-			*line = ft_strsub((const char*)*stock, 0, tmp - *stock);
-			ft_memmove(*stock, tmp + 1, ft_strlen(tmp));
+			*line = ft_strsub((const char*)*static_, 0, tmp - *static_);
+			ft_memmove(*static_, tmp + 1, ft_strlen(tmp));
 			return (1);
 		}
 	}
 	return (0);
 }
 
-static int		ft_read(int fd, char **stock, char **str)
+static int		ft_read(int fd, char **static_, char **str)
 {
 	int			ret;
 	char		buf[BUFF_SIZE + 1];
@@ -38,15 +38,15 @@ static int		ft_read(int fd, char **stock, char **str)
 	while ((ret = read(fd, buf, BUFF_SIZE)))
 	{
 		buf[ret] = '\0';
-		tmp = *stock;
-		if (*stock)
+		tmp = *static_;
+		if (*static_)
 		{
-			*stock = ft_strjoin((char const*)tmp, (char const*)buf);
+			*static_ = ft_strjoin((char const*)tmp, (char const*)buf);
 			free(tmp);
 		}
 		else
-			*stock = ft_strdup((const char*)buf);
-		if (ft_check_static(stock, str))
+			*static_ = ft_strdup((const char*)buf);
+		if (ft_check_static(static_, str))
 			return (1);
 	}
 	return (0);
@@ -62,8 +62,7 @@ int				get_next_line(const int fd, char **line)
 		return (-1);
 	if (gnl[fd] && ft_check_static(&gnl[fd], line))
 		return (1);
-	res = ft_read(fd, &gnl[fd], line);
-	if (res != 0)
+	if ((res = ft_read(fd, &gnl[fd], line)) != 0)
 		return (res);
 	if (gnl[fd] == NULL || gnl[fd][0] == '\0')
 		return (0);
@@ -71,3 +70,78 @@ int				get_next_line(const int fd, char **line)
 	gnl[fd] = NULL;
 	return (1);
 }
+
+/*
+**					commentaires
+**
+**				{check si je suis a la fin de ma ligne}
+**
+**	static int		ft_check_static(char **static_, char **line)
+**	{
+**		char		*tmp;
+**
+**		tmp = NULL;
+**		if (*static_)
+**		{
+**			if ((tmp = ft_strchr((const char*)*static_, '\n')))
+**			{
+**				*line = ft_strsub((const char*)*static_, 0, tmp - *static_);
+**				ft_memmove(*static_, tmp + 1, ft_strlen(tmp));
+**							// memmove de tmp sur mon static
+**				return (1);
+**			}
+**		}
+**		return (0);
+**	}
+**
+**
+**				{ read }
+**
+**	static int		ft_read(int fd, char **static_, char **str)
+**	{
+**		int			ret;
+**		char		buf[BUFF_SIZE + 1];
+**					// je gere pas le cas d'un buffer de 10 000 000
+**		char		*tmp;
+**
+**		while ((ret = read(fd, buf, BUFF_SIZE)))
+**		{
+**			buf[ret] = '\0';
+**			tmp = *static_;
+**			if (*static_)
+**			{
+**				*static_ = ft_strjoin((char const*)tmp, (char const*)buf);
+**				free(tmp); // pour que je puise garder les anciennes lines
+**			}
+**			else
+**				*static_ = ft_strdup((const char*)buf);
+**						// pour ma premiere line
+**			if (ft_check_static(static_, str))
+**				return (1);
+**		}
+**		return (0);
+**	}
+**
+**
+**	int				get_next_line(const int fd, char **line)
+**	{
+**		static char	*gnl[FD_VL];
+**	// tres beau tableau de char mdr(ah changer ça pour une liste dans l'avenir)
+**		int			res;
+**		char		test_read[BUFF_SIZE];
+**	//pour tester si le fd est bien ouvert pour la lecture
+**
+**		if (fd < 0 || !line || (read(fd, test_read, 0) < 0))
+**			return (-1);
+**		if (gnl[fd] && ft_check_static(&gnl[fd], line))
+**			return (1);
+**		res = ft_read(fd, &gnl[fd], line); // read returnera un int sur res
+**		if (res != 0)
+**			return (res);
+**		if (gnl[fd] == NULL || gnl[fd][0] == '\0')
+**			return (0); // fin des testes logics sur res
+**		*line = gnl[fd];
+**		gnl[fd] = NULL;
+**		return (1);
+**	}
+*/
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,19 +6,19 @@
 /*   By: dnetto <dnetto@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/10 11:59:21 by dnetto            #+#    #+#             */
-/*   Updated: 2017/05/15 15:56:46 by dnetto           ###   ########.fr       */
+/*   Updated: 2017/05/15 19:27:38 by dnetto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include "libft/libft.h"
+# include "/Users/dnetto/Desktop/get_next_line/libft/libft.h"
 # include <sys/types.h>
 # include <sys/uio.h>
 # include <unistd.h>
 # define FD_VL 65636
-# define BUFF_SIZE 4100000
+# define BUFF_SIZE  32
 
 int	get_next_line(const int fd, char **line);
 
